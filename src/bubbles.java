928e15a7f08884b40944749da35fba24b05fb4a0
Sonic <52312596+SonicCreator@users.noreply.github.com>
2021-02-17 12:40:54 +0100

    Add files via upload

---
+++ src/bubbles.java
@@ -0,0 +1,6 @@
+public class bubbles {
+    public static void main(String[] args)
+    {
+        System.out.print("hello, i am Wbubbler, your friend ! bub is cool. tell friends to install me i am funny. wbubbler 1.0.3 ");
+    }
+}
