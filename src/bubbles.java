74fbbf60b7177da89504398f65dd7c9f54743983
Wbubbler <75879906+Wbubbler@users.noreply.github.com>
2021-02-24 16:57:56 +0100

    Update bubbles.java
    Just changed it to be more like the original.

--- src/bubbles.java
+++ src/bubbles.java
@@ -1,6 +1,9 @@
 public class bubbles {
     public static void main(String[] args)
     {
-        System.out.print("hello, i am Wbubbler, your friend ! bub is cool. tell friends to install me i am funny. wbubbler 1.0.3 ");
+        System.out.println("hello, i am Wbubbler, your friend !");
+        System.out.println("bub is cool.");
+        System.out.println("tell friends to install me i am funny.");
+        System.out.println("wbubbler 1.0.3 ");
     }
 }
