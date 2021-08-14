c6fa2a4a3de28549b0d97e90083daf0424ef8eea
Wbubbler <75879906+Wbubbler@users.noreply.github.com>
2021-08-14 08:09:51 +0200

    e

---
+++ src/Wbubbler.java
@@ -0,0 +1,7 @@
+public class Wbubbler {
+    public static void wbubbler()
+    {
+        System.out.println("hello, i am Wbubbler, your friend ! \nbub is cool. \ntell friends to install me i am funny. \nwbubbler 1.0.3 ");
+    }
+}
+
--- src/bubbles.java
+++
@@ -1,9 +0,0 @@
-public class bubbles {
-    public static void main(String[] args)
-    {
-        System.out.println("hello, i am Wbubbler, your friend !");
-        System.out.println("bub is cool.");
-        System.out.println("tell friends to install me i am funny.");
-        System.out.println("wbubbler 1.0.3 ");
-    }
-}
